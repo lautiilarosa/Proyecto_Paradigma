2c4c28a6d4aa30fb733dd9d42d468837a5d2443e
Paula Martínez <martinezpaula1488@gmail.com>
2023-09-02 18:43:31 -0300

    Subo cambios en el menú y la funcion mostrarCaracteristicas

--- VinoSabroso/src/Entidades/TipoUva.java
+++ VinoSabroso/src/Entidades/TipoUva.java
@@ -2,13 +2,14 @@ package Entidades;
 public enum TipoUva {
     CABERNET_SAUVIGNON, MERLOT, TEMPRANILLO, GARNACHA, PINOT_NOIR, BONARDA, PINOT_GRIS, CHARDONNAY, SAUVIGNON_BLANC, VERDEJO, ALBARIÑO, GODELLO, MALBEC, MOSCATEL_ALEJANDRIA, CABERNET_FRANC;
 
-    public void mostrarUvas() {
+    public static void mostrarUvas() {
         int contador = 1;
         for (TipoUva tipoUva : TipoUva.values()) {
             System.out.println(contador + ": " + tipoUva);
             contador++;
         }
     }
+
 }
 
 
