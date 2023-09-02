5844b120cb2964181dc3545326b6afb600fa7bb1
paulisuden <spaulinaabril@gmail.com>
2023-09-02 18:08:56 -0300

    Uvas disponibles

--- VinoSabroso/src/Main.java
+++ VinoSabroso/src/Main.java
@@ -1,8 +1,8 @@
 import Entidades.* ;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         //TipoUva tipoUva = TipoUva.MERLOT;
         //tipoUva.mostrarUvas();
@@ -13,7 +13,7 @@ public class Main {
         int mes = scan.nextInt();
         // ---------- Menu ----------
         boolean exit = false;
-        while (exit==false){
+        while (exit == false) {
             System.out.println("Bienvenido a Vino Sabroso!");
             System.out.println(" ");
             System.out.println("1 - Ingresar un tipo de uva");
@@ -22,9 +22,8 @@ public class Main {
             System.out.println("4 - Ver Características de un vino");
             System.out.println("5 - Salir");
             int eleccion = scan.nextInt();
-            switch (eleccion){
+            switch (eleccion) {
                 case 1:
-
 
 
                 case 5:
@@ -34,6 +33,34 @@ public class Main {
                     System.out.println("Ingrese una opción válida, por favor");
             }
         }
+
+    }
+
+    public static ArrayList<String> calcularUvasDisponibles(int mes) {
+        ArrayList<String> uvasDisponibles = new ArrayList<>();
+        //ArrayList<String> uvasNoDisponibles = new ArrayList<>();
+        if (mes == 1 | mes == 6 | mes == 7 | mes == 11 | mes == 12) {
+            uvasDisponibles.add("No hay uvas disponibles para cosechar este mes");
+        } else if (mes == 2) {
+            uvasDisponibles.add("Cabernet Sauvignon"); uvasDisponibles.add("Malbec");
+        } else if (mes == 3) {
+            uvasDisponibles.add("Cabernet Sauvignon"); uvasDisponibles.add("Pinot Gris"); uvasDisponibles.add("Sauvignon Blanc"); uvasDisponibles.add("Malbec"); uvasDisponibles.add("Merlot");
+        } else if (mes == 4) {
+            uvasDisponibles.add("Tempranillo"); uvasDisponibles.add("Merlot");
+        } else if (mes == 5) {
+            uvasDisponibles.add("Tempranillo");
+        } else if (mes == 8) {
+            uvasDisponibles.add("Moscatel de Alejandría"); uvasDisponibles.add("Pinot Noir");uvasDisponibles.add("Chardonnay"); uvasDisponibles.add("Godello");
+        } else if (mes == 9) {
+            uvasDisponibles.add("Granacha"); uvasDisponibles.add("Pinot Noir"); uvasDisponibles.add("Verdejo"); uvasDisponibles.add("Albariño"); uvasDisponibles.add("Moscatel de Alejandría"); uvasDisponibles.add("Godello"); uvasDisponibles.add("Cabernet Franc");
+        } else if (mes == 10) {
+            uvasDisponibles.add("Granacha"); uvasDisponibles.add("Albariño"); uvasDisponibles.add("Cabernet Franc");
+        }
+        return uvasDisponibles;
+
+        // para consutltar uvas disponibles:
+        //ArrayList<String> uvasDisponibles = Main.calcularUvasDisponibles(mes);
+        //System.out.println(uvasDisponibles);
     }
 }
 
