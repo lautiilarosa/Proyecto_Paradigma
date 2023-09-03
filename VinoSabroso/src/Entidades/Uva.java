51187fa002848595bcb4c896450af9b750db1c7b
Lautaro Larosa <lautarolarosacn56@gmail.com>
2023-09-03 15:23:27 -0300

    Borrar Clase uva y utilizar un enum

--- VinoSabroso/src/Entidades/Uva.java
+++ VinoSabroso/src/Entidades/Uva.java
@@ -1,45 +1,79 @@
 package Entidades;
 
-public class Uva {
-    private TipoUva tipoUva;
-    private String maridaje;
-    private String cuerpo;
-    private String aromaIntensidad;
-    private String sabor;
-    private String aparienciaVisual;
-    private String lugarCosecha;
-    private String epocaCosecha;
-    private int gradoAlcohol;
+public enum Uva {
+    CABERNET_SAUVIGNON("Tinto", "Mendoza", new String[] { "Febrero", "Marzo" }, new int[] { 12, 36 }, "Rojo y Intensidad media", "Grosella,intenso", "Completo", "Frutas Oscuras","Carnes Rojas Asadas"),
+    MERLOT("Tinto","Mendoza",new String[] { "Marzo","Abril" },new int[] {6,24},"Rojo Rubí","Ciruelay Intensidad media","Medio","Suave,Frutal","Pollo,cerdo"),
+    TEMPRANILLO("Tinto","Mendoza",new String[] {"Abril","Mayo"}, new int[] {6,48}, "Rojo Granate", "Frutas Rojas y Intensidad media-alta", "Medio", "Frutas Maduras", "Jamón,Cordero"),
+    GRANACHA("Tinto","España",new String[] {"Septiembre","Octubre"},new int[] {0},"Rojo Brillante","Frutas Rojas y Intensidad media","Medio", "Bayas,especias","Carnes a la pariila,embutidos"),
+    PINOT_NOIR("Tinto","Francia",new String[] {"Agosto","Septiembre"},new int[] {6,24}, "Rojo Claro","Frutas rojas y Intensidad baja-media","ligero","Delicado Frutal","Aves,pescados"),
+    BONARDA("Tinto","Argentina (este mendocino),Italia",new String[] {"Agosto","Septiembre"}, new int[] {0}, "Rojo Rubí", "Fruta Roja y Intensidad Media","Medio","Sabor Suave","Quesos Duros(vinos más envejecidos) y Pastas(vinos jóvenes"),
+    PINOT_GRIS("Blanco","Francia,Alemania",new String[] {"Febrero","Marzo"},new int[] {3,6},"Amarillo Oscuro","Cítricos y Intensidad Moderada","Medio a pleno","Mezcla de frutas tropicales(melón y mango)","Carnes Blancas,comida asiática"),
+    CHARDONNAY("Blanco","Francia,Estados Unidos",new String[] {"Agosto"},new int[] {0},"Amarillo Pálido","Frutos frescos y Intensidad Moderada","Ligero y Fresco","Frutas Blancas y tropicales","Platos de Mariscos"),
+    SAUVIGNONG_BLANC("Blanco","Francia,Nueva Zelanda",new String[] {"Febrero","Marzo"},new int[] {1},"Tono amarillo pálido","Refrescante y Intensidad media-alta","Ligero a medio","Notas herbales y vegetales","Mariscos,pescados"),
+    VERDEJO("Blanco","España",new String[] {"Septiembre"},new int[] {3,6},"Amarillo Verdoso","Aroma frutal intenso","Ligero a medio","Frutos secos y miel","Pollo ligero y Mariscos"),
+    ALBARIÑO("Blanco","España,Argentina",new String[] {"Septiembre","Octubre"},new int[] {6},"Amarillo Pálido","Flores blancas y Intensidad moderada","Ligero a medio","Sabores Cítricos y frutales","Frutas del mar y Carnes Blancas"),
+    GODELLO("BLANCO","España",new String[] {"Agosto","Septiembre"},new int[] {3,6},"Color Amarillo Duradero","Manzana y Intensidad Media","Medio-Alto","Frutas blancas y amarillas","Pescados Blancos y quesos suaves"),
+    MALBEC("Tinto","Francia,Argentina",new String[] {"Febrero","Marzo"},new int[] {3,12},"Rojo Oscuro y Púrpura","Frutas Negras y Intensidad Media-Alta","Medio-Completo","Frutas negras(ciruelas y moras","Carnes rojas a la parilla"),
+    CABERNET_FRANC("Tinto","Francia,Argentina",new String[] {"Septiembre","Octubre"},new int[] {6,18},"Rojo Rubí(Vinos Jóvenes) y Morado Oscuro(Vinos envejecidos)","Vainilla y Intensidad Media-Alta","Medio-Alto","Frutos rojos (frambuesas y cerezas)","Carnes rojas y aves"),
+    MOSCATEL_ALEJANDRIA("Blanco","España,Italia",new String[] {"Agosto","Septiembre"},new int[] {3,6},"Color Dorado","Flor de azahary Intensidad Alta","Ligero-Medio","Sabor Canela o Vainilla","Frutas Frescas y secas");
 
-        //Constructor
+    private final String tipoUva;
+    private final String lugarCosecha;
+    private final String[] fechaCosecha;
+    private final int[] fechaCrianza;
+    private final String aparienciaVisual;
+    private final String aromasIntensidad;
+    private final String cuerpo;
+    private final String sabor;
+    private final String maridaje;
 
-    public Uva(TipoUva tipoUva, String maridaje, String cuerpo, String aromaIntensidad, String sabor, String aparienciaVisual,String lugarCosecha, String epocaCosecha, int gradoAlcohol){
+    private Uva(String tipoUva, String lugarCosecha, String[] fechaCosecha, int[] fechaCrianza, String aparienciaVisual, String aromasIntensidad, String cuerpo, String sabor,String maridaje) {
         this.tipoUva = tipoUva;
-        this.maridaje = maridaje;
-        this.cuerpo = cuerpo;
-        this.aromaIntensidad = aromaIntensidad;
-        this.sabor = sabor;
-        this.aparienciaVisual = aparienciaVisual;
         this.lugarCosecha = lugarCosecha;
-        this.epocaCosecha = epocaCosecha;
-        this.gradoAlcohol = gradoAlcohol;
+        this.fechaCosecha = fechaCosecha;
+        this.fechaCrianza = fechaCrianza;
+        this.aparienciaVisual = aparienciaVisual;
+        this.aromasIntensidad = aromasIntensidad;
+        this.cuerpo = cuerpo;
+        this.sabor = sabor;
+        this.maridaje = maridaje;
     }
 
-    //Getters
-    public TipoUva getTipoUva() {return tipoUva;}
-    public String getMaridaje() {return maridaje;}
+    // Métodos getter para acceder a los atributos
+    public String getTipoUva() {
+        return tipoUva;
+    }
 
-    public String getCuerpo(){return cuerpo;}
+    public String getLugarCosecha() {
+        return lugarCosecha;
+    }
 
-    public String getAromaIntensidad(){return aromaIntensidad;}
+    public String[] getFechaCosecha() {
+        return fechaCosecha;
+    }
 
-    public String getSabor() {return sabor;}
+    public int[] getFechaCrianza() {
+        return fechaCrianza;
+    }
 
-    public String getAparienciaVisual() {return aparienciaVisual;}
+    public String getAparienciaVisual() {
+        return aparienciaVisual;
+    }
 
-    public String getLugarCosecha(){return lugarCosecha;}
+    public String getAromasIntensidad() {
+        return aromasIntensidad;
+    }
 
-    public String getEpocaCosecha() {return epocaCosecha;}
-    public int getGradoAlcohol() {return gradoAlcohol;}
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public String getMaridaje(){
+        return maridaje;
+    }
 
 }
--- VinoSabroso/src/Servicios/UvaServicios.java
+++ VinoSabroso/src/Servicios/UvaServicios.java
@@ -8,10 +8,9 @@ public class UvaServicios {
         System.out.println("Su maridaje: "+uva.getMaridaje());
         System.out.println("Su sabor: "+uva.getSabor());
         System.out.println("Su apariencia: "+uva.getAparienciaVisual());
-        System.out.println("Su aroma e intensidad: "+uva.getAromaIntensidad());
+        System.out.println("Su aroma e intensidad: "+uva.getAromasIntensidad());
         System.out.println("Su cuerpo: "+uva.getCuerpo());
-        System.out.println("Su grado de alcohol: "+uva.getGradoAlcohol());
-        System.out.println("El lugar de cosecha: "+uva.getLugarCosecha());
-        System.out.println("La fecha de cosecha: "+uva.getEpocaCosecha());
+        //System.out.println("El lugar de cosecha: "+uva.getLugarCosecha());
+        //System.out.println("La fecha de cosecha: "+uva.getFechaCosecha());
     }
 }
