9b398ca0f9ea12f72fe6685db762c1425eb4bd60
paulisuden <spaulinaabril@gmail.com>
2023-09-04 10:42:46 -0300

    Ingresar uva

--- VinoSabroso/src/Main.java
+++ VinoSabroso/src/Main.java
@@ -2,6 +2,7 @@ import Entidades.* ;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class Main {
@@ -60,9 +61,8 @@ public class Main {
                     ArrayList<String> uvasDisponibles = Main.calcularUvasDisponibles(mes);
                     System.out.println("La uvas disponibles para este mes son: ");
                     System.out.println(uvasDisponibles);
-
+                    int probando = ingresarUva();
                     break;
-
                 case 2:
                     consultarEstadosVinos();
                     break;
@@ -87,6 +87,41 @@ public class Main {
             }
         }
     }
+
+    public static int ingresarUva() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("A continuación elija la opción de la uva que desea ingresar:");
+        System.out.println("1.\tCabernet Sauvignon: \n" +
+                "2.\tMerlot\n" +
+                "3.\tTempranillo\n" +
+                "4.\tGranacha: \n" +
+                "5.\tPinot Noir\n" +
+                "6.\tBonarda\n" +
+                "7.\tPinot Gris\n" +
+                "8.\tChardonnay\n" +
+                "9.\tSauvignon Blanc\n" +
+                "10.\tVerdejo\n" +
+                "11.\tAlbariño\n" +
+                "12.\tGodello\n" +
+                "13.\tMoscatel\n" +
+                "14.\tCabernet Franc\n" +
+                "15.\tMalbec\n");
+        int opcion = 0;
+        boolean entradaValida = false;
+        while (!entradaValida) {
+            try {
+                opcion = scan.nextInt();
+                if (opcion >= 1 && opcion <= 15) { entradaValida = true;
+                } else if (opcion < 1 | opcion > 15) { entradaValida = false; }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese un número entre 1 y 15.");
+                scan.nextLine(); // "Limpiar" el búfer de entrada para evitar un bucle infinito.
+            }
+        }
+    return opcion;
+    }
+
     public static ArrayList<String> calcularUvasDisponibles(int mes) {
         ArrayList<String> uvasDisponibles = new ArrayList<>();
         //ArrayList<String> uvasNoDisponibles = new ArrayList<>();
